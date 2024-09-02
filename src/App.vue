0e6fc7976762f1c34dfc77b830879fbaa333c264
王建 <jian.wang@versa-ai.com>
2024-09-02 13:22:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>App根组件</h1>
+  </div>
+</template>
+<script lang="ts" setup name="App">
+const str = '我爱你祖国'
+</script>
+<style scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
