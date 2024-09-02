ad9dfc0a8c36b68ffc70630f1ed2904a672b4359
王建 <jian.wang@versa-ai.com>
2024-09-02 13:54:58 +0800

    feat: 新增一个标题

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <h1>App根组件</h1>
+    <h1>新增一个标题</h1>
   </div>
 </template>
 <script lang="ts" setup name="App">
-for (leti=0;i<100;i++)
-{
-  console.log(i);
+for (let i = 0; i < 100; i++) {
+  console.log(i)
 }
 </script>
 <style scoped>
