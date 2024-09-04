99d607befb9071f04b981805bfb7ab6b80beb866
王建 <jian.wang@versa-ai.com>
2024-09-04 13:05:29 +0800

    feat: 菜单图标完成

--- src/layout/menu/index.vue
+++ src/layout/menu/index.vue
@@ -3,9 +3,11 @@
     <template v-for="item in menuList" :key="item.path">
       <!-- 无子菜单 -->
       <template v-if="!item.children">
-        <el-menu-item v-if="!item.meta.hidden" :index="item.path">
+        <el-menu-item v-if="!item.meta.hidden" :index="item.path" @click="goRoute">
           <template #title>
-            <span>标&nbsp;</span>
+            <el-icon>
+              <component :is="item.meta.icon"></component>
+            </el-icon>
             <span>{{ item.meta.title }}</span>
           </template>
         </el-menu-item>
@@ -14,7 +16,9 @@
       <template v-if="item.children && item.children.length == 1">
         <el-menu-item v-if="!item.children[0].meta.hidden" :index="item.children[0].path">
           <template #title>
-            <span>标&nbsp;</span>
+            <el-icon>
+              <component :is="item.children[0].meta.icon"></component>
+            </el-icon>
             <span>{{ item.children[0].meta.title }}</span>
           </template>
         </el-menu-item>
@@ -22,6 +26,9 @@
       <!-- 两个及以上的子菜单 -->
       <el-sub-menu v-if="item.children && item.children.length > 1" :index="item.path">
         <template #title>
+          <el-icon>
+            <component :is="item.meta.icon"></component>
+          </el-icon>
           <span>{{ item.meta.title }}</span>
         </template>
         <Menu :menuList="item.children"></Menu>
@@ -32,5 +39,9 @@
 <script lang="ts" setup name="Menu">
 // 获取父组件传递过来的全部路由数组
 defineProps(['menuList'])
+// 点击菜单的回调
+const goRoute = (vc: any) => {
+  console.log(vc.path)
+}
 </script>
 <style scoped></style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,6 +8,7 @@ export const constantRoute = [
     meta: {
       title: '登录', // 菜单标题
       hidden: true, // 代表路由标题在菜单中是否隐藏 true：隐藏 false：不隐藏
+      icon: 'Promotion', // 菜单文字左侧的图标，支持 element-plus 全部图标
     },
   },
   {
@@ -18,6 +19,7 @@ export const constantRoute = [
     meta: {
       title: 'layout',
       hidden: false, // 代表路由标题在菜单中是否隐藏 true：隐藏 false：不隐藏
+      icon: 'Avatar',
     },
     children: [
       {
@@ -25,15 +27,8 @@ export const constantRoute = [
         compentnt: () => import('@/views/home/index.vue'),
         meta: {
           title: '首页',
-          hidden: true,
-        },
-      },
-      {
-        path: '/ceshi',
-        compentnt: () => import('@/views/home/index.vue'),
-        meta: {
-          title: '测试',
           hidden: false,
+          icon: 'HomeFilled',
         },
       },
     ],
@@ -45,6 +40,7 @@ export const constantRoute = [
     meta: {
       title: '404',
       hidden: true,
+      icon: 'DocumentDelete',
     },
   },
   {
@@ -55,6 +51,7 @@ export const constantRoute = [
     meta: {
       title: '任意路由',
       hidden: true,
+      icon: 'DataLine',
     },
   },
 ]
