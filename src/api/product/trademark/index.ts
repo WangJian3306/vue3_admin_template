de18aa1f2b88e201f8040606568b6fe61034f404
王建 <jian.wang@versa-ai.com>
2024-09-06 21:15:47 +0800

    feat: 品牌管理模块数据展示

---
+++ src/api/product/trademark/index.ts
@@ -0,0 +1,13 @@
+// 品牌管理模块接口
+import request from '@/utils/request'
+// 品牌管理模块接口地址
+enum API {
+  // 获取已有品牌接口
+  TRADEMARK_URL = '/admin/product/baseTrademark/',
+}
+
+// 获取已有品牌数据接口方法
+// page：获取第几页数据，默认第一页
+// limit：每页中数据条数
+export const reqHasTrademark = (page: number, limit: number) =>
+  request.get<any, any>(API.TRADEMARK_URL + `${page}/${limit}`)
--- src/views/product/tradmark/index.vue
+++ src/views/product/tradmark/index.vue
@@ -11,11 +11,25 @@
       ---width：这一这列的宽度
       ---align：这一列文本对其方式
      -->
-    <el-table border style="margin: 10px 0">
-      <el-table-column label="序号" width="80px" align="center"></el-table-column>
-      <el-table-column label="品牌名称" align="center"></el-table-column>
-      <el-table-column label="品牌LOGO" align="center"></el-table-column>
-      <el-table-column label="操作" align="center"></el-table-column>
+    <el-table border style="margin: 10px 0" :data="trademarkArr">
+      <el-table-column label="序号" width="80px" align="center" type="index"></el-table-column>
+      <!-- el-table-column 默认展示数据用的是 div -->
+      <el-table-column label="品牌名称" align="center">
+        <template v-slot="{ row }">
+          <pre style="color: red">{{ row.tmName }}</pre>
+        </template>
+      </el-table-column>
+      <el-table-column label="品牌LOGO" align="center">
+        <template v-slot="{ row }">
+          <img :src="row.logoUrl" style="width: 100px; height: 100px" />
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center">
+        <template v-slot>
+          <el-button type="primary" size="small" icon="Edit"></el-button>
+          <el-button type="primary" size="small" icon="Delete"></el-button>
+        </template>
+      </el-table-column>
     </el-table>
     <!-- 分页器组件
       pagination
@@ -33,13 +47,14 @@
       :size="size"
       :background="true"
       layout="prev, pager, next, jumper, ->, sizes, total"
-      :total="400"
+      :total="total"
     />
   </el-card>
 </template>
 <script lang="ts" setup name="Tradmark">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import type { ComponentSize } from 'element-plus'
+import { reqHasTrademark } from '@/api/product/trademark'
 
 // 当前页码
 let pageNo = ref<number>(1)
@@ -47,5 +62,25 @@ let pageNo = ref<number>(1)
 let limit = ref<number>(3)
 // 分页器大小
 const size = ref<ComponentSize>('default')
+// 存储已有品牌数据总数
+let total = ref<number>(0)
+// 存储已有品牌的数据
+let trademarkArr = ref<any>([])
+
+// 获取已有品牌的接口封装维一个函数：在任何情况下获取数据，调用此函数即可
+const getHasTrademark = async () => {
+  const result = await reqHasTrademark(pageNo.value, limit.value)
+  if (result.code == 200) {
+    // 存储已有品牌总个数
+    total.value = result.data.total
+
+    trademarkArr.value = result.data.records
+  }
+}
+
+// 组件挂载完毕，发送请求获取数据
+onMounted(() => {
+  getHasTrademark()
+})
 </script>
 <style scoped></style>
