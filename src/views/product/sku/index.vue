94994224ea86f40d60e1e3ebe7fd6e89edab2bed
王建 <jian.wang@versa-ai.com>
2024-09-30 14:43:52 +0800

    feat: 商品详情静态搭建

--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -25,7 +25,7 @@
             @click="updateSale(row)"
           ></el-button>
           <el-button type="primary" size="small" icon="Edit" @click="updateSku"></el-button>
-          <el-button type="primary" size="small" icon="InfoFilled"></el-button>
+          <el-button type="primary" size="small" icon="InfoFilled" @click="findSku"></el-button>
           <el-button type="primary" size="small" icon="Delete"></el-button>
         </template>
       </el-table-column>
@@ -40,6 +40,49 @@
       @size-change="handleSizeChange"
       @current-change="getHasSku"
     />
+    <!-- 抽屉组件：展示商品详情 -->
+    <el-drawer v-model="drawer">
+      <!-- 标题部分 -->
+      <template #header>
+        <h4>查看商品的详情</h4>
+      </template>
+      <template #default>
+        <el-row style="margin: 10px 0px">
+          <el-col :span="6">名称</el-col>
+          <el-col :span="18">华为MateXT</el-col>
+        </el-row>
+        <el-row style="margin: 10px 0px">
+          <el-col :span="6">描述</el-col>
+          <el-col :span="18">华为YYDS</el-col>
+        </el-row>
+        <el-row style="margin: 10px 0px">
+          <el-col :span="6">价格</el-col>
+          <el-col :span="18">6999</el-col>
+        </el-row>
+        <el-row style="margin: 10px 0px">
+          <el-col :span="6">平台属性</el-col>
+          <el-col :span="18">
+            <el-tag v-for="item in 10" :key="item" style="margin: 5px 5px">{{ item }}</el-tag>
+          </el-col>
+        </el-row>
+        <el-row style="margin: 10px 0px">
+          <el-col :span="6">销售属性</el-col>
+          <el-col :span="18">
+            <el-tag v-for="item in 10" :key="item" style="margin: 5px 5px">{{ item }}</el-tag>
+          </el-col>
+        </el-row>
+        <el-row style="margin: 10px 0px">
+          <el-col :span="6">商品图片</el-col>
+          <el-col :span="18">
+            <el-carousel :interval="4000" type="card" height="200px">
+              <el-carousel-item v-for="item in 6" :key="item">
+                <h3 text="2xl" justify="center">{{ item }}</h3>
+              </el-carousel-item>
+            </el-carousel>
+          </el-col>
+        </el-row>
+      </template>
+    </el-drawer>
   </el-card>
 </template>
 <script lang="ts" setup name="SKU">
@@ -54,6 +97,9 @@ let pageNo = ref<number>(1)
 let pageSize = ref<number>(10)
 let total = ref<number>(0)
 let skuArr = ref<SkuData[]>([])
+
+// 控制抽屉显示与隐藏
+let drawer = ref<boolean>(false)
 
 // 组件挂载完毕
 onMounted(() => {
@@ -109,5 +155,26 @@ const updateSku = () => {
     message: '程序员在努力的建设中...',
   })
 }
+
+// 查看商品详情按钮的回调
+const findSku = () => {
+  drawer.value = true
+}
 </script>
-<style scoped></style>
+<style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
