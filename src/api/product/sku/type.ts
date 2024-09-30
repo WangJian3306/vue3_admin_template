0924d855689e8601f58e08ea6b06a1d8c845b6f4
王建 <jian.wang@versa-ai.com>
2024-09-30 13:38:29 +0800

    feat: 展示已有SKU

---
+++ src/api/product/sku/type.ts
@@ -0,0 +1,49 @@
+export interface ResponseData {
+  code: number
+  message: string
+  ok: boolean
+}
+
+// 定义SKU对象的ts类型
+// SKU 平台属性
+export interface SkuAttrValue {
+  attrId: string | number // 平台属性ID
+  valueId: string | number // 属性值ID
+}
+
+// SKU 销售属性
+export interface SkuSaleAttrValue {
+  saleAttrId: string | number // 销售属性ID
+  saleAttrValueId: string | number // 销售属性值ID
+}
+
+// SKU 数据
+export interface SkuData {
+  category3Id: string | number // 三级分类的ID
+  spuId: string | number // 已有的SPU的ID
+  tmId: string | number // SPU 品牌ID
+  skuName: string // SKU 名字
+  price: string | number // SKU 价格
+  weight: string | number // SKU 重量
+  skuDesc: string // SKU 描述
+  skuAttrValueList: SkuAttrValue[] // 平台属性
+  skuSaleAttrValueList: SkuSaleAttrValue[] // 销售属性
+  skuDefaultImg: string // 售卖SKU图片地址
+}
+
+// 获取 SKU 接口返回的数据 ts 类型
+export interface SkuResponseData extends ResponseData {
+  data: {
+    records: SkuData[]
+    total: number
+    size: number
+    current: number
+    orders: []
+    optimizeCountSql: boolean
+    hitCount: boolean
+    countId: null
+    maxLimit: null
+    searchCount: true
+    pages: boolean
+  }
+}
--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -1,13 +1,29 @@
 <template>
   <el-card>
-    <el-table border style="margin: 10px 0px">
+    <el-table border style="margin: 10px 0px" :data="skuArr">
       <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
-      <el-table-column label="名称" show-overflow-tooltip width="150px"></el-table-column>
-      <el-table-column label="描述" show-overflow-tooltip></el-table-column>
-      <el-table-column label="默认图片" width="150px"></el-table-column>
-      <el-table-column label="重量" width="150px"></el-table-column>
-      <el-table-column label="价格" width="150px"></el-table-column>
-      <el-table-column label="操作" width="250px" fixed="right"></el-table-column>
+      <el-table-column
+        label="名称"
+        show-overflow-tooltip
+        width="150px"
+        prop="skuName"
+      ></el-table-column>
+      <el-table-column label="描述" show-overflow-tooltip prop="skuDesc"></el-table-column>
+      <el-table-column label="默认图片" width="150px">
+        <template v-slot="{ row }">
+          <img :src="row.skuDefaultImg" alt="" style="height: 100px; width: 100px" />
+        </template>
+      </el-table-column>
+      <el-table-column label="重量" width="150px" prop="weight"></el-table-column>
+      <el-table-column label="价格" width="150px" prop="price"></el-table-column>
+      <el-table-column label="操作" width="250px" fixed="right">
+        <template v-slot="{ row }">
+          <el-button type="primary" size="small" icon="Top"></el-button>
+          <el-button type="primary" size="small" icon="Edit"></el-button>
+          <el-button type="primary" size="small" icon="InfoFilled"></el-button>
+          <el-button type="primary" size="small" icon="Delete"></el-button>
+        </template>
+      </el-table-column>
     </el-table>
     <el-pagination
       v-model:current-page="pageNo"
@@ -15,17 +31,43 @@
       :page-sizes="[10, 20, 30, 40]"
       :background="true"
       layout="prev, pager, next, jumper, ->, sizes, total "
-      :total="400"
+      :total="total"
       @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
+      @current-change="getHasSku"
     />
   </el-card>
 </template>
 <script lang="ts" setup name="SKU">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+// 引入请求方法
+import { reqSkuList } from '@/api/product/sku'
+import type { SkuData, SkuResponseData } from '@/api/product/sku/type'
 // 分页器当前页码
 let pageNo = ref<number>(1)
 // 每一页展示几条数据
 let pageSize = ref<number>(10)
+let total = ref<number>(0)
+let skuArr = ref<SkuData[]>([])
+
+// 组件挂载完毕
+onMounted(() => {
+  getHasSku()
+})
+
+// 获取已有SKU
+const getHasSku = async (pager = 1) => {
+  // 当前分页器的页码
+  pageNo.value = pager
+  const result: SkuResponseData = await reqSkuList(pageNo.value, pageSize.value)
+  if (result.code === 200) {
+    total.value = result.data.total
+    skuArr.value = result.data.records
+  }
+}
+
+// 分页器下拉菜单发生变化触发
+const handleSizeChange = () => {
+  getHasSku()
+}
 </script>
 <style scoped></style>
