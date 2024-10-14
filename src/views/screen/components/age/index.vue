f6b6f620e592bb785926a3a28af95683bfa9fdab
王建 <jian.wang@versa-ai.com>
2024-10-14 16:22:51 +0800

    feat: 数据大屏实时游客统计

---
+++ src/views/screen/components/age/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>年龄比例</div>
+</template>
+<script lang="ts" setup name="Age"></script>
+<style scoped></style>
---
+++ src/views/screen/components/sex/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>性别比例</div>
+</template>
+<script lang="ts" setup name="Sex"></script>
+<style scoped></style>
